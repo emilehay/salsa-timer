ebaa9250c3ab17dabb7aa2bb44fa38d873f86b97
Emile Hay <emile.hay@multichoice.com>
2020-12-15 14:09:53 +0200

    [Feature] Added the buttongroup component and made some style updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import Timer from './components/Timer/Timer';
+import ButtonGroup from './components/Button-group/Button-group';
 
 function App() {
   return (
     <div className="App">
+      <ButtonGroup />
       <Navbar />
       <Timer />
     </div>
---
+++ src/components/Button-group/Button-group.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Button-group.scss';
+
+const ButtonGroup = () => {
+  return (
+    <div class="button-group">
+      <button>Pomodoro</button>
+      <button>Short rest</button>
+      <button>Long rest</button>
+    </div>
+  )
+}
+
+export default ButtonGroup
